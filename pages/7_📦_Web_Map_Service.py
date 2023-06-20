18732cfb48c63621f9508880ddd4547d575742e4
Quach Dong Thang <quachdongthang@gmail.com>
2023-06-20 08:33:41 +0700

    a
    a

--- pages/7_📦_Web_Map_Service.py
+++ pages/7_📦_Web_Map_Service.py
@@ -6,16 +6,16 @@ st.set_page_config(layout="wide")
 
 st.sidebar.info(
     """
-    - Web App URL: <https://streamlit.geemap.org>
-    - GitHub repository: <https://github.com/giswqs/streamlit-geospatial>
+    - Web: <https://becagis.streamlit.app/>
+    - GitHub: <https://github.com/thangqd/becagis_streamlit>
     """
 )
 
 st.sidebar.title("Contact")
 st.sidebar.info(
     """
-    Qiusheng Wu: <https://wetlands.io>
-    [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu)
+    Thang Quach: <https://thangqd.github.io>
+    [GitHub](https://github.com/thangqd) | [Twitter](https://twitter.com/thangqd) | [LinkedIn](https://www.linkedin.com/in/thangqd)
     """
 )
 
@@ -31,8 +31,7 @@ def app():
     st.markdown(
         """
     This app is a demonstration of loading Web Map Service (WMS) layers. Simply enter the URL of the WMS service 
-    in the text box below and press Enter to retrieve the layers. Go to https://apps.nationalmap.gov/services to find 
-    some WMS URLs if needed.
+    in the text box below and press Enter to retrieve the layers.
     """
     )
 
@@ -43,9 +42,9 @@ def app():
 
     with row1_col2:
 
-        esa_landcover = "https://services.terrascope.be/wms/v2"
+        becagis_opendata = "https://opendata.hcmgis.vn/geoserver/ows"
         url = st.text_input(
-            "Enter a WMS URL:", value="https://services.terrascope.be/wms/v2"
+            "Enter a WMS URL:", value="https://opendata.hcmgis.vn/geoserver/ows"
         )
         empty = st.empty()
 
@@ -53,22 +52,22 @@ def app():
             options = get_layers(url)
 
             default = None
-            if url == esa_landcover:
-                default = "WORLDCOVER_2020_MAP"
+            if url == becagis_opendata:
+                default = "geonode:global_submarine_cable_line"
             layers = empty.multiselect(
                 "Select WMS layers to add to the map:", options, default=default
             )
-            add_legend = st.checkbox("Add a legend to the map", value=True)
-            if default == "WORLDCOVER_2020_MAP":
-                legend = str(leafmap.builtin_legends["ESA_WorldCover"])
-            else:
-                legend = ""
-            if add_legend:
-                legend_text = st.text_area(
-                    "Enter a legend as a dictionary {label: color}",
-                    value=legend,
-                    height=200,
-                )
+            # add_legend = st.checkbox("Add a legend to the map", value=True)
+            # if default == "Global Submarine Cables":
+            #     legend = str(leafmap.builtin_legends["Global Submarine Cables"])
+            # else:
+            #     legend = ""
+            # if add_legend:
+            #     legend_text = st.text_area(
+            #         "Enter a legend as a dictionary {label: color}",
+            #         value=legend,
+            #         height=200,
+            #     )
 
         with row1_col1:
             m = leafmap.Map(center=(36.3, 0), zoom=2)
@@ -78,9 +77,9 @@ def app():
                     m.add_wms_layer(
                         url, layers=layer, name=layer, attribution=" ", transparent=True
                     )
-            if add_legend and legend_text:
-                legend_dict = ast.literal_eval(legend_text)
-                m.add_legend(legend_dict=legend_dict)
+            # if add_legend and legend_text:
+            #     legend_dict = ast.literal_eval(legend_text)
+            #     m.add_legend(legend_dict=legend_dict)
 
             m.to_streamlit(height=height)
 
