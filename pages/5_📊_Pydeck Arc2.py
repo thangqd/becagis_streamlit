aa1de55b2ffeb059976c45fab37dae7d25340ea0
Quach Dong Thang <quachdongthang@gmail.com>
2023-06-28 16:49:42 +0700

    a
    a

--- pages/5_📊_Pydeck Arc2.py
+++ pages/5_📊_Pydeck Arc2.py
@@ -20,9 +20,11 @@ st.sidebar.info(
 )
 
 DATA_URL = "https://raw.githubusercontent.com/thangqd/becagis_streamlit/main/data/airlines_vn.csv"
+DATA_URL = "./data/airlines_vn.csv"
+
 # DATA_URL = "https://raw.githubusercontent.com/ajduberstein/sf_public_data/master/bay_area_commute_routes.csv"
 df = pd.read_csv(DATA_URL)
-st.write (df)
+# st.write (df)
 
 GREEN_RGB = [0, 255, 0, 40]
 RED_RGB = [240, 100, 0, 40]
@@ -40,7 +42,7 @@ arc_layer = pdk.Layer(
     get_source_color=RED_RGB,
     get_target_color=GREEN_RGB,
     pickable=True,
-    auto_highlight=True
+    auto_highlight=True,
 )
 
 view_state = pdk.ViewState(latitude=10.045180, longitude=105.78841, bearing=45, pitch=50, zoom=8)
@@ -49,9 +51,11 @@ view_state = pdk.ViewState(latitude=10.045180, longitude=105.78841, bearing=45, 
 
 # TOOLTIP_TEXT = {"html": "{id} jobs <br /> Home of commuter in red; work location in green"}
 # r = pdk.Deck(arc_layer, initial_view_state=view_state, tooltip=TOOLTIP_TEXT)
+r = pdk.Deck(arc_layer, initial_view_state=view_state)
+
 
 st.pydeck_chart(pdk.Deck(
-    arc_layer,
+    layers=[arc_layer],
     initial_view_state=view_state
     # layers=[hexagon,scatterplot,heatmap]
     # tooltip=TOOLTIP_TEXT
