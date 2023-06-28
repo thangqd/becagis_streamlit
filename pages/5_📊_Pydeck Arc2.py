5edc59b5353d7f04b920df067b1d51c872954e19
Quach Dong Thang <quachdongthang@gmail.com>
2023-06-28 16:02:41 +0700

    a
    a

--- pages/5_📊_Pydeck Arc2.py
+++ pages/5_📊_Pydeck Arc2.py
@@ -21,6 +21,7 @@ st.sidebar.info(
 
 DATA_URL = "https://raw.githubusercontent.com/thangqd/becagis_streamlit/main/data/airlines_vn.csv"
 df = pd.read_csv(DATA_URL)
+st.write (df)
 
 GREEN_RGB = [0, 255, 0, 40]
 RED_RGB = [240, 100, 0, 40]
@@ -29,31 +30,24 @@ arc_layer = pdk.Layer(
     "ArcLayer",
     data=df,
     # get_width="S000 * 2",
-    get_width=2,
+    # get_width=2,
     get_source_position=["lon_f", "lat_f"],
     get_target_position=["lon_t", "lat_t"],
     get_tilt=15,
     get_source_color=RED_RGB,
     get_target_color=GREEN_RGB,
     pickable=True,
-    auto_highlight=True,
+    auto_highlight=True
 )
 
+view_state = pdk.ViewState(latitude=10.045180, longitude=105.78841, bearing=45, pitch=50, zoom=8)
 
-def hex_to_rgb(h):
-    h = h.lstrip("#")
-    return tuple(int(h[i : i + 2], 16) for i in (0, 2, 4))
-
-
-df["color"] = df["color"].apply(hex_to_rgb)
-
-
-view_state = pdk.ViewState(latitude=10.045180, longitude=105.78841, zoom=10)
-
+# TOOLTIP_TEXT = {"html": "{id} jobs <br /> Home of commuter in red; work location in green"}
+# r = pdk.Deck(arc_layer, initial_view_state=view_state, tooltip=TOOLTIP_TEXT)
 
 st.pydeck_chart(pdk.Deck(
-    layers=[arc_layer],
-    initial_view_state=view_state,
+    arc_layer,
+    initial_view_state=view_state
     # layers=[hexagon,scatterplot,heatmap]
-    tooltip={"text": "{name}"}
+    # tooltip=TOOLTIP_TEXT
 ))
