7ac15b5b818c34a0a76415509fe381d96648f8f8
Quach Dong Thang <quachdongthang@gmail.com>
2023-06-28 11:23:03 +0700

    qq
    q

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -23,13 +23,14 @@ st.title("Marker Cluster")
 with st.expander("See source code"):
     with st.echo():
 
-        m = leafmap.Map(center=[10.045180, 105.78841], zoom=8)       
-        watersupply_mekong = './data/wqi.csv'
+        m = leafmap.Map(center=[10.045180, 105.78841], zoom=0)       
+        # watersupply_mekong = './data/wqi.csv'
+        airports =  './data/airports.csv'
         provinces = './data/vn_provinces.geojson'
 
         m.add_geojson(provinces, layer_name='Vietnam Province')
         m.add_points_from_xy(
-            watersupply_mekong,
+            airports,
             x="longitude",
             y="latitude",
             # color_column='province',
