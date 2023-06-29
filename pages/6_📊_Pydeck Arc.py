5e0237c8d81080ec01c85614bf80f8075670bb0a
Quach Dong Thang <quachdongthang@gmail.com>
2023-06-29 15:31:52 +0700

    Update 6_📊_Pydeck Arc.py
    A

--- pages/6_📊_Pydeck Arc.py
+++ pages/6_📊_Pydeck Arc.py
@@ -24,11 +24,11 @@ DATA_URL = "https://raw.githubusercontent.com/thangqd/becagis_streamlit/main/dat
 
 df = pd.read_csv(DATA_URL)
 # st.write (df)
-
+WHITE_RGB = [255, 255, 255, 40]
 GREEN_RGB = [0, 255, 0, 40]
 RED_RGB = [240, 100, 0, 40]
 
-arc_layer = pdk.Layer(
+air_lines = pdk.Layer(
     "ArcLayer",
     data=df,
     get_width="0.1",
@@ -36,8 +36,8 @@ arc_layer = pdk.Layer(
     get_source_position=["lon_f", "lat_f"],
     get_target_position=["lon_t", "lat_t"],
     get_tilt=15,
-    get_source_color=RED_RGB,
-    get_target_color=GREEN_RGB,
+    get_source_color=WHITE_RGB,
+    get_target_color=WHITE_RGB,
     pickable=True,
     auto_highlight=True,
 )
@@ -45,11 +45,24 @@ arc_layer = pdk.Layer(
 view_state = pdk.ViewState(latitude=10.045180, longitude=105.78841, bearing=0, pitch=20, zoom=1)
 
 TOOLTIP_TEXT = {"html": "{src} to {dst} "}
-r = pdk.Deck(arc_layer, initial_view_state=view_state)
+# r = pdk.Deck(arc_layer, initial_view_state=view_state)
+
+ARIPORTS_URL = (
+    "https://raw.githubusercontent.com/thangqd/becagis_streamlit/main/data/airports.csv"  
+)
+
+air_ports = pdk.Layer(
+    'ScatterplotLayer',     # Change the `type` positional argument here
+    ARIPORTS_URL,
+    get_position=["longitude", "latitude"],
+    auto_highlight=True,
+    get_radius=20000,          # Radius is given in meters
+    get_fill_color=[180, 0, 200, 140],  # Set an RGBA value for fill
+    pickable=True)
 
 
 st.pydeck_chart(pdk.Deck(
-    layers=[arc_layer],
+    layers=[air_lines,air_ports],
     initial_view_state=view_state,
     map_style=None,
     tooltip=TOOLTIP_TEXT
