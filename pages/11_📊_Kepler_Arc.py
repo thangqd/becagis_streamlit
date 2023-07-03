942716503764cf2727b9333a1803e2c28837acfe
Quach Dong Thang <quachdongthang@gmail.com>
2023-07-03 11:01:07 +0700

    a
    a

--- pages/11_📊_Kepler_Arc.py
+++ pages/11_📊_Kepler_Arc.py
@@ -24,7 +24,7 @@ st.sidebar.info(
 airports =  'https://raw.githubusercontent.com/thangqd/becagis_streamlit/main/data/airports.csv'
 df_airports = pd.read_csv(airports)
 
-airlines =  'https://raw.githubusercontent.com/thangqd/becagis_streamlit/main/data/airlines_oneway.csv'
+airlines =  'https://raw.githubusercontent.com/thangqd/becagis_streamlit/main/data/airlines.csv'
 df_airlines = pd.read_csv(airlines)
 
 m = KeplerGl(height=600)
