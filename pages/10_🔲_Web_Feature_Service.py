8d4a0118c6c8f1f375b394f42d20c64e5b060d59
Quach Dong Thang <quachdongthang@gmail.com>
2023-08-14 11:17:06 +0700

    a
    a

--- pages/10_🔲_Web_Feature_Service.py
+++ pages/10_🔲_Web_Feature_Service.py
@@ -2,6 +2,8 @@ import streamlit as st
 import leafmap.foliumap as leafmap
 import pandas as pd
 import requests
+import os
+import re
 
 st.set_page_config(layout="wide")
 
@@ -48,12 +50,52 @@ if 'clicked' not in st.session_state:
 
 def wfs_button(url):
     st.session_state.clicked = True
-   
+    uri = url +'/wfs?request=GetCapabilities'
+    wfs = requests.get(uri, stream=True, allow_redirects=True, verify = False)
+    filename = "wfs.xml"  
+    if  (wfs.status_code == 200):               
+        f = open(filename, 'wb')                           
+        for chunk in wfs.iter_content(chunk_size = 1024):
+            if not chunk:
+                break
+            f.write(chunk)                                                            
+        f.close()
+    if wfs is not None:              
+        getcapabilities = open(filename, 'r') 
+        data = getcapabilities.read()
+        getcapabilities.close()
+        os.remove(filename)
+        server_title_regex = r'<ows:Title>(.+?)</ows:Title>|<ows:Title/>'
+        
+        server_abstract_regex = r'<ows:Abstract>(.+?)</ows:Abstract>|<ows:Abstract/>'
+        
+        server_title = re.findall(server_title_regex,data,re.DOTALL)
+        server_abstract = re.findall(server_abstract_regex,data,re.DOTALL)
+
+        layer_name_regex = r'<Name>(.+?)</Name>'
+        layer_title_regex = r'<Title>(.+?)</Title>|<Title/>'
+
+
+        layer_name = re.findall(layer_name_regex,data,re.DOTALL)
+        layer_title = re.findall(layer_title_regex,data,re.DOTALL)
+
+        if len(server_title)>0:
+            with col1:
+                st.write('Server Title: ', server_title[0])
+
+        if len (server_abstract)>0:
+            with col1:
+                st.write('Server Abstract: ', server_abstract[0].replace('&#13;',''))
+        
+        if len(layer_name)>0:
+            st.write(layer_name)
+        if len(layer_title)>0:
+            st.write(layer_title)
+        
+
 
 st.button('Load WFS Layers', on_click=wfs_button(selected_url))
 
 if st.session_state.clicked:
     # The message and nested widget will remain on the page
-    uri = df_filter.url +'/wfs?request=GetCapabilities'
-    wfs = requests.get(uri, stream=True, allow_redirects=True, verify = False)
-    st.write(wfs)
+    pass
