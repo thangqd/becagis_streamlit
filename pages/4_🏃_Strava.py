cc765e861b071df9973059cdf24ecb1364d9e7c5
Quach Dong Thang <quachdongthang@gmail.com>
2023-08-22 11:44:29 +0700

    a
    a

--- pages/4_🏃_Strava.py
+++ pages/4_🏃_Strava.py
@@ -1,15 +1,52 @@
-import gpxpy
 import pandas as pd
 import folium
-from streamlit_folium import st_folium
-from folium.map import Marker, Template
+from streamlit_folium import st_folium, folium_static
+from folium.plugins import AntPath
 import streamlit as st
 
-tiger  = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/tiger.csv"
-df = pd.read_csv(tiger)
+st.set_page_config(layout="wide")
 
-latitude = sum(p[0] for p in df)/len(df)
-longitude = sum(p[1] for p in df)/len(df)
-myMap = folium.Map(location=[latitude,longitude],tiles="stamenterrain", width = 600, zoom_start=12)
-folium.PolyLine(df, color="red", weight=2.5, opacity=1).add_to(myMap)
-st_folium (myMap)
+st.sidebar.info(
+    """
+    - Web: [BecaGIS Streamlit](https://becagis.streamlit.app)
+    - GitHub: [BecaGIS Streamlit](https://github.com/thangqd/becagis_streamlit) 
+    """
+)
+
+st.sidebar.title("Contact")
+st.sidebar.info(
+    """
+    Thang Quach: [BecaGIS Homepage](https://becagis.vn/?lang=en) | [My Homepage](https://thangqd.github.io) | [GitHub](https://github.com/thangqd) | [LinkedIn](https://www.linkedin.com/in/thangqd)
+    """
+)
+
+# Reference: https://dwtkns.com/srtm30m/
+
+st.title("Strava Route")
+col1, col2 = st.columns([1,30])
+with col1:
+    st.image("./data/images/strava.png", width = 30)
+with col2:
+    st.write("[Lenny Maughan's Strava Route GADM](https://www.strava.com/athletes/7019519)")
+
+
+
+tiger  = "https://raw.githubusercontent.com/thangqd/becagis_streamlit/main/data/csv/tiger.csv"
+df = pd.read_csv(tiger)
+points = df[["lat", "lon"]]
+center_lat = points["lat"].mean()
+center_lon = points["lon"].mean()
+
+myMap = folium.Map(location=[center_lat,center_lon], tiles="stamenterrain", width = 600, zoom_start=12)
+folium.PolyLine(points, color="red", weight=2.5, opacity=1).add_to(myMap)
+
+# ant_path = AntPath(
+#     locations=points,
+#     dash_array=[1, 10],
+#     delay=1000,
+#     color='#7590ba',
+#     pulse_color='#3f6fba'
+# ).add_to(myMap)
+
+
+folium_static(myMap)
