4a7a47abb8c69ca2c065f82476ec4190f4142e23
Quach Dong Thang <quachdongthang@gmail.com>
2023-08-22 22:08:49 +0700

    a
    a

--- pages/4_🏃_Strava_Art_3.py
+++ pages/4_🏃_Strava_Art_3.py
@@ -18,17 +18,15 @@ st.sidebar.info(
     Thang Quach: [BecaGIS Homepage](https://becagis.vn/?lang=en) | [My Homepage](https://thangqd.github.io) | [GitHub](https://github.com/thangqd) | [LinkedIn](https://www.linkedin.com/in/thangqd)
     """
 )
-st.title("Kepler Time Series")
+st.title("Strava Art")
 
 
-wqi_timeseries =  'https://raw.githubusercontent.com/thangqd/becagis_streamlit/main/data/csv/tiger.csv'
-df_wqi = pd.read_csv(wqi_timeseries)
-df_wqi["ds"] =  pd.to_datetime(df_wqi["date"]) # convert Date field to     
-df_wqi["ds"] = df_wqi['date'].dt.strftime('%Y-%m-%d %H:%M:%S')
+tiger_timeseries =  'https://raw.githubusercontent.com/thangqd/becagis_streamlit/main/data/csv/tiger.csv'
+df_tiger = pd.read_csv(tiger_timeseries)
 
 m = KeplerGl(height=600)
 m.add_data(
-    data=df_wqi, name="Time Series Data"
+    data=df_tiger, name="Time Series Data"
 )  
 
 keplergl_static(m, center_map=True)
