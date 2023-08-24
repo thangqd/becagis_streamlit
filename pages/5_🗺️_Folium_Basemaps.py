2387c773fbf574c7f0a03ff14f784ae15310edf0
Quach Dong Thang <quachdongthang@gmail.com>
2023-08-24 13:29:42 +0700

    a
    a

--- pages/5_🗺️_Folium_Basemaps.py
+++ pages/5_🗺️_Folium_Basemaps.py
@@ -27,9 +27,6 @@ def app():
     """
     )
 
-    with st.expander("See demo"):
-        st.image("https://i.imgur.com/0SkUhZh.gif")
-
     row1_col1, row1_col2 = st.columns([3, 1])
     width = 800
     height = 600
