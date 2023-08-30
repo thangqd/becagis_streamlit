78dc094f5da57e7b97b5823a7ac466952c73c05b
Quach Dong Thang <quachdongthang@gmail.com>
2023-08-30 10:46:13 +0700

    a
    a

--- pages/4_⏲️Time Series Data.py
+++ pages/4_⏲️Time Series Data.py
@@ -24,14 +24,14 @@ st.sidebar.info(
 st.title("Time Series Data")
 
 
-config_file = "./data/kepler/strava_config.json"
-with open(config_file, "r",encoding="utf-8") as f:
-    config = json.load(f)
+# config_file = "./data/kepler/strava_config.json"
+# with open(config_file, "r",encoding="utf-8") as f:
+#     config = json.load(f)
 
-geolenny_maughan_json = pd.read_json('https://raw.githubusercontent.com/thangqd/becagis_streamlit/main/data/strava/lenny_maughan.geojson')
-lenny_maughan_map = KeplerGl(data={"trip_data": geolenny_maughan_json}, config = config, height=600)
+# geolenny_maughan_json = pd.read_json('https://raw.githubusercontent.com/thangqd/becagis_streamlit/main/data/strava/lenny_maughan.geojson')
+# lenny_maughan_map = KeplerGl(data={"trip_data": geolenny_maughan_json}, config = config, height=600)
 
-keplergl_static(lenny_maughan_map,  center_map=True)
+# keplergl_static(lenny_maughan_map,  center_map=True)
 
 # df = pd.read_csv('https://raw.githubusercontent.com/thangqd/becagis_streamlit/main/data/strava/lenny_maughan.csv')
 # config_file = "./data/kepler/strava_config.json"
@@ -54,24 +54,24 @@ keplergl_static(lenny_maughan_map,  center_map=True)
 
 
 #############################################
-# # df = pd.read_csv('https://raw.githubusercontent.com/thangqd/becagis_streamlit/main/data/csv/nyc.csv')
-# # config_file = "./data/kepler/nyc_config.json"
+df = pd.read_csv('https://raw.githubusercontent.com/thangqd/becagis_streamlit/main/data/csv/nyc.csv')
+config_file = "./data/kepler/nyc_config.json"
 
-# with open(config_file, "r",encoding="utf-8") as f:
-#      config = json.load(f)
+with open(config_file, "r",encoding="utf-8") as f:
+     config = json.load(f)
 
-# df["elevation"] = 0
-# geo_json = dict(type="FeatureCollection", features=[])
-# geo_json["features"]
+df["elevation"] = 0
+geo_json = dict(type="FeatureCollection", features=[])
+geo_json["features"]
 
-# for trip in df.VendorID.unique():
-#     feature = dict(type="Feature", geometry=None, properties=dict(VendorID=str(trip)))
-#     feature["geometry"] = dict(type="LineString", coordinates=df.loc[df.VendorID==trip, ["pickup_longitude", "pickup_latitude", "elevation", "tpep_pickup_datetime"]].to_records(index=False).tolist())
-#     geo_json["features"].append(feature)
+for trip in df.VendorID.unique():
+    feature = dict(type="Feature", geometry=None, properties=dict(VendorID=str(trip)))
+    feature["geometry"] = dict(type="LineString", coordinates=df.loc[df.VendorID==trip, ["pickup_longitude", "pickup_latitude", "elevation", "tpep_pickup_datetime"]].to_records(index=False).tolist())
+    geo_json["features"].append(feature)
 
-# geo_json["features"].append(feature)
+geo_json["features"].append(feature)
 
-# my_map = KeplerGl(data={"trip_data": geo_json}, config = config, height=600)
-# # my_map = KeplerGl(data= geo_json, height=600)
-# keplergl_static(my_map,  center_map=True)
+my_map = KeplerGl(data={"trip_data": geo_json}, config = config, height=600)
+# my_map = KeplerGl(data= geo_json, height=600)
+keplergl_static(my_map,  center_map=True)
 
