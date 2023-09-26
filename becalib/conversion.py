338139eeb78160bc91f04c3db529ab1ca1c9e591
Quach Dong Thang <quachdongthang@gmail.com>
2023-09-26 10:16:20 +0700

    a
    a

--- becalib/conversion.py
+++
@@ -1,9 +0,0 @@
-import geopandas as gpd
-import pandas as pd
-from shapely import wkt
-# df = pd.read_csv("d:\\Projects\\2023\\Satra\\Buildings\\317_buildings.csv", nrows  =1000)
-df = pd.read_csv("d:\\Projects\\2023\\Satra\\Buildings\\317_buildings.csv")
-df['geometry'] = df['geometry'].apply(wkt.loads)
-gdf = gpd.GeoDataFrame(df, crs='epsg:4326')
-gdf.to_file("d:\\Projects\\2023\\Satra\\Buildings\\317_buildings.geojson", driver="GeoJSON") 
-# print(gdf.head)
